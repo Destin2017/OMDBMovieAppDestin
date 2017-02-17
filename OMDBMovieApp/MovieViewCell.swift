474d7b254b41e076383a5e084545ca582c83ec27
Samuel Maina Karumba <karumbas@ke.ibm.com>
2017-02-17 20:07:48 +0300

    first commit

---
+++ OMDBMovieApp/MovieViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieViewCell.swift
+//  OMDBMovieApp
+//
+//  Created by Karumba Samuel on 17/02/2017.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
